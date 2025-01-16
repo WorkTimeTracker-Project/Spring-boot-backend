7679627468604e1590701b47ba8721834dce5866
rezokip <ozerkip@hotmail.de>
2025-01-16 09:14:09 +0100

    initialized project

---
+++ src/main/java/com/ozer/workTimeTracker/WorkTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ozer.workTimeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozer/workTimeTracker/WorkTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozer.workTimeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
