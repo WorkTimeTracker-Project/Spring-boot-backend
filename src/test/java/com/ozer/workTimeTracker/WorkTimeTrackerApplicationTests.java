838b89f1abb0f6bcb9448b0954a080fe1e8d487c
rezokip <ozerkip@hotmail.de>
2025-01-17 14:57:38 +0100

    tests

--- src/test/java/com/ozer/workTimeTracker/WorkTimeTrackerApplicationTests.java
+++ src/test/java/com/ozer/workTimeTracker/WorkTimeTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ozer.workTimeTracker;
+/*package com.ozer.workTimeTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class WorkTimeTrackerApplicationTests {
 	}
 
 }
+*/
