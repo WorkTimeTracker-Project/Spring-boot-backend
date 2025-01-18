2e060b7fe6ce4ecedba872954cf4fc32c8536bd3
rezokip <ozerkip@hotmail.de>
2025-01-18 17:24:09 +0100

    added logging and multiple cors endpoints

--- src/main/java/com/ozer/workTimeTracker/CorsConfig.java
+++ src/main/java/com/ozer/workTimeTracker/CorsConfig.java
@@ -1,13 +1,15 @@
 package com.ozer.workTimeTracker;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed.origins}")
-    private String allowedOrigins;
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/ozer/workTimeTracker/WorkTimeTrackerApplication.java
+++ src/main/java/com/ozer/workTimeTracker/WorkTimeTrackerApplication.java
@@ -1,13 +1,38 @@
 package com.ozer.workTimeTracker;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WorkTimeTrackerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkTimeTrackerApplication.class, args);
+	}
+
+	@Value("${cors.allowed.origins}")
+	private String allowedOrigins;
+
+	@Value("${spring.datasource.url}")
+	private String databaseUrl;
+
+	@Value("${spring.datasource.username}")
+	private String databaseUser;
+
+	@Value("${spring.datasource.password}")
+	private String databasePW;
+
+	@PostConstruct
+	public void printApplicationProperties() {
+		System.out.println("alowedOrigins: " + allowedOrigins);
+		System.out.println("databaseUrl: " + databaseUrl);
+		System.out.println("databaseUser: " + databaseUser);
+		System.out.println("databasePW: " + databasePW);
 	}
 
 }
